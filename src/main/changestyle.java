077e53390a6669efa060e335c59f5b18e5613fd5
Ebrahem Samer <Soltan_Algaram41@yahoo.com>
2017-12-09 20:39:31 +0200

    Beginning

---
+++ src/main/changestyle.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+public class changestyle {
+    
+    public void chabgelock(){
+   try{
+       UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
+   }catch(Exception e){
+     JOptionPane.showMessageDialog(null,e.getMessage(),"Error",2);
+   }
+}
+}
